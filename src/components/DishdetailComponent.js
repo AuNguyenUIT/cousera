38916de7f072a0443ab94d413acf121b5698a41a
AuNguyenUIT <aunguyenuit@gmail.com>
2020-09-14 17:42:32 +0700

    update DishdetailComponet.js

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,33 +7,42 @@ export default function DishDetail(props) {
   const renderDish = (dish) => {
     if (dish != null) {
       return (
-        <>
-          <div className="col-sm-12 col-md-5 m-1">
-            <Card>
-              <CardImg src={dish.image} alt={dish.name} />
-              <CardBody className="text-left">
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-              </CardBody>
-            </Card>
-          </div>
-          <div className="col-sm-12 col-md-5 m-1 text-left">
-            <h4>Comments</h4>
-            {dish.comments.map((comment) => {
-              const date = new Date(comment.date);
-              return (
-                <Fragment key={comment.id}>
-                  <p>{comment.comment}</p>
-                  <p>
-                    -- {comment.author}, {date.toDateString()}
-                  </p>
-                </Fragment>
-              );
-            })}
-          </div>
-        </>
+        <div className="col-sm-12 col-md-5 m-1">
+          <Card>
+            <CardImg src={dish.image} alt={dish.name} />
+            <CardBody className="text-left">
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </div>
       );
     } else return <div></div>;
   };
-  return renderDish(selectedDish);
+  const renderComment = (dish) => {
+    if (dish != null) {
+      return (
+        <div className="col-sm-12 col-md-5 m-1 text-left">
+          <h4>Comments</h4>
+          {dish.comments.map((comment) => {
+            const date = new Date(comment.date);
+            return (
+              <Fragment key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>
+                  -- {comment.author}, {date.toDateString()}
+                </p>
+              </Fragment>
+            );
+          })}
+        </div>
+      );
+    } else return <div></div>;
+  };
+  return (
+    <Fragment>
+      {renderDish(selectedDish)}
+      {renderComment(selectedDish)}
+    </Fragment>
+  );
 }
