21e4bff338bea687de10a2c4256cffdcf1109d8a
AuNguyenUIT <aunguyenuit@gmail.com>
2020-09-14 14:31:49 +0700

    Componets part1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
+import { MenuComponent } from "./components/MenuComponent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Pisces</NavbarBrand>
         </div>
       </Navbar>
+      <MenuComponent />
     </div>
   );
 }
